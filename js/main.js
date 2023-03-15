56d60e8f418fcf9c5753187c389ad0f5956cbbdf
shesler <98080551+shesler@users.noreply.github.com>
2023-03-15 14:46:50 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,26 +1,21 @@
 let eventBus = new Vue()
 
 Vue.component('column', {
-    // колонки
     template: `
  
         <div class="columns">
             <newCard></newCard>
+            <p class="error" v-for="error in errors">{{ error }}</p>
             
             <p class="head_tabl">NOTES</p>
-            
-        <p class="error" v-for="error in errors">{{ error }}</p>
-                <column_1 :column_1="column_1"></column_1>
+                <column_1 :column_2="column_2" :column_1="column_1" @blocked="block"></column_1>
                 <column_2 :column_2="column_2"></column_2>
                 <column_3 :column_3="column_3"></column_3>
         </div>
     `,
-
-
-
-
     data() {
         return {
+            isBlock: false,
             column_1: [],
             column_2: [],
             column_3: [],
@@ -29,14 +24,17 @@ Vue.component('column', {
 
     },
     mounted() {
+        this.column_1 = JSON.parse(localStorage.getItem('column_1')) || [];
+        this.column_2 = JSON.parse(localStorage.getItem('column_2')) || [];
+        this.column_3 = JSON.parse(localStorage.getItem('column_3')) || [];
         eventBus.$on('addColumn_1', ColumnCard => {
-
             if (this.column_1.length < 3) {
                 this.errors.length = 0
                 this.column_1.push(ColumnCard)
+                this.saveColumn_1();
             } else {
                 this.errors.length = 0
-                this.errors.push()
+                this.errors.push("Follow previous notes!")
             }
         })
         eventBus.$on('addColumn_2', ColumnCard => {
@@ -44,16 +42,45 @@ Vue.component('column', {
                 this.errors.length = 0
                 this.column_2.push(ColumnCard)
                 this.column_1.splice(this.column_1.indexOf(ColumnCard), 1)
+                this.saveColumn_1();
+                this.saveColumn_2();
+
             } else {
                 this.errors.length = 0
-                this.errors.push()
+                this.errors.push("Follow previous notes!")
             }
         })
         eventBus.$on('addColumn_3', ColumnCard => {
+            ColumnCard.date = new Date().toLocaleString()
             this.column_3.push(ColumnCard)
             this.column_2.splice(this.column_2.indexOf(ColumnCard), 1)
+            this.saveColumn_2();
+            this.saveColumn_3();
 
         })
+    },
+
+    watch: {
+        isBlock(val) {
+            if(val === true) {
+
+            }
+        }
+    },
+
+    methods: {
+        block() {
+            this.isBlock = true;
+        }, // момент когда второй заполнен и в первом есть > 50% заметки
+        saveColumn_1(){
+            localStorage.setItem('column_1', JSON.stringify(this.column_1));
+        },
+        saveColumn_2(){
+            localStorage.setItem('column_2', JSON.stringify(this.column_2));
+        },
+        saveColumn_3(){
+            localStorage.setItem('column_3', JSON.stringify(this.column_3));
+        }
     }
 })
 
@@ -71,18 +98,18 @@ Vue.component('newCard', {
         <div class="form_control">
                 
             <div class="form_name">
-                <input required type="text" v-model="name" id="name" placeholder="Введите название заметки"/>
+                <input required type="text" v-model="name" id="name" placeholder="Note title"/>
             </div>
             
-            <input required type="text"  v-model="point_1" placeholder="Первый пункт"/>
+            <input required type="text"  v-model="point_1" placeholder="First point"/>
             <br>
-            <input required type="text"  v-model="point_2" placeholder="Второй пункт"/>
+            <input required type="text"  v-model="point_2" placeholder="Second point"/>
             <br>
-            <input required type="text"  v-model="point_3" placeholder="Третий пункт"/> 
+            <input required type="text"  v-model="point_3" placeholder="Third point"/> 
             <br>
-            <input  type="text"  v-model="point_4"  placeholder="Четвертый пункт"/>
+            <input  type="text"  v-model="point_4"  placeholder="Fourth point"/>
             <br>
-             <input type="text" v-model="point_5"  placeholder="Пятый пункт"/>
+             <input type="text" v-model="point_5"  placeholder="Fifth point"/>
         </div>
             <div class="form_control">
                 <button class="btn">SAVE</button>
@@ -102,7 +129,6 @@ Vue.component('newCard', {
         }
     },
     methods: {
-
         Submit() {
             let card = {
                 name: this.name,
@@ -114,7 +140,6 @@ Vue.component('newCard', {
                     {name: this.point_5, completed: false}
                 ],
                 date: null,
-                // date: null,
                 status: 0,
                 errors: [],
             }
@@ -127,7 +152,6 @@ Vue.component('newCard', {
             this.point_5 = null
         }
     }
-
 })
 
 Vue.component('column_1', {
@@ -165,17 +189,35 @@ Vue.component('column_1', {
     },
     methods: {
         TaskCompleted(ColumnCard, task) {
-            task.completed = true
-            ColumnCard.status += 1
+
+            if (task.completed === false) {
+                task.completed = true
+                ColumnCard.status += 1
+            }
+
             let count = 0
-            for(let i = 0; i < 2; i++){
-                count++
+            for (let i = 0; i < 5; ++i) {
+                if (ColumnCard.points[i].name !== null) {
+                    count++;
+                }
             }
-            if ((ColumnCard.status / count) * 100 >= 100) {
+
+            if ((ColumnCard.status / count) * 100 >= 50 && this.column_2.length < 5) {
                 eventBus.$emit('addColumn_2', ColumnCard)
-                this.column_1.splice(this.column_1.indexOf(ColumnCard), 1)
+                this.column_1.splice(this.column_1.indexOf(ColumnCard), 0)
             }
-        },
+
+            if (this.column_2.length === 5) {
+                if(this.column_1.length > 0) {
+                    this.column_1.forEach(item => {
+                        item.points.forEach(item => {
+                            item.completed = false;
+                            ColumnCard.card = 0;
+                        })
+                    })
+                }
+            }
+        }
     },
 })
 
@@ -207,17 +249,19 @@ Vue.component('column_2', {
     },
     methods: {
         TaskCompleted(ColumnCard, task) {
-            task.completed = true
-            ColumnCard.status += 1
+            if (task.completed === false){
+                task.completed = true
+                ColumnCard.status += 1
+            }
             let count = 0
-            for(let i = 0; i < 5; i++){
-                count++
+            for (let i = 0; i < 5; ++i) {
+                if (ColumnCard.points[i].name !== null) {
+                    count++;
+                }
             }
             if (( ColumnCard.status / count) * 100 >= 100 ) {
                 eventBus.$emit('addColumn_3', ColumnCard)
                 ColumnCard.date = new Date().toLocaleString()
-
-
             }
         }
     }
@@ -236,9 +280,8 @@ Vue.component('column_3', {
                         <li>
                         {{ task.name }}
                         </li>
-                    </ul><br>
-                    
-                        <p>{{ card.date }}</p>
+                    </ul>
+                    <p>{{ card.date }}</p>
                 </div>
             </div>
         </section>
